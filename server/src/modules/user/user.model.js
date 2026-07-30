c07f90d989daa9a28b2a634062006f62dd89da5f
Yograj Rijal <yograjrijal926@gmail.com>
2026-07-30 12:30:03 +0545

    refactor(user): improve user model

---
+++ server/src/modules/user/user.model.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import { ROLES } from "../shared/constants/roles.constant.js";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add a name"],
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: [true, "Please add an email"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: [true, "Please add a password"],
+        },
+
+        avatar: {
+            type: String,
+            default: null,
+        },
+
+        role: {
+            type: String,
+            enum: Object.values(ROLES),
+            default: ROLES.STAFF,
+        },
+
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        verifyOtp: String,
+
+        verifyOtpExpiresAt: {
+            type: Number,
+            default: 0,
+        },
+
+        resetOtp: String,
+
+        resetOtpExpiresAt: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.index({ email: 1 });
+
+export default mongoose.model("User", userSchema);
