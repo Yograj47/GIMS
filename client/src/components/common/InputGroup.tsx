bea59f170955368530fb7ce0c618e42be14bc230
Yograj Rijal <yograjrijal926@gmail.com>
2026-08-13 13:02:58 +0545

    Fixed bugs found during deployment

--- client/src/components/common/InputGroup.tsx
+++ client/src/components/common/InputGroup.tsx
@@ -1,4 +1,13 @@
-export function InputGroup({ label, disabled, value, onChange }: any) {
+import { type ChangeEvent } from 'react';
+
+export interface InputGroupProps {
+    label: string;
+    disabled?: boolean;
+    value: string | number;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export function InputGroup({ label, disabled, value, onChange }: InputGroupProps) {
     return (
         <div className="space-y-3">
             <label className="text-[10px] font-black text-slate-400 uppercase tracking-[0.15em] ml-1">{label}</label>
